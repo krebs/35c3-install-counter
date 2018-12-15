023aa506f0c99f271cc07b8ec7c056a6bc632701
makefu <github@syntax-fehler.de>
2018-12-16 00:09:19 +0100

    init trivial microcontroller code

---
+++ uc/src/Main.ino
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+const int buttonPin = D4;    // the number of the pushbutton pin
+
+// Variables will change:
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.println("Boot");
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        Serial.println("HIGH");
+      } else {
+        Serial.println("LOW");
+      }
+    }
+  }
+  lastButtonState = reading;
+}
